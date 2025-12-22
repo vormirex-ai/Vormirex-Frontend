bb0fa547319398470edab1a0e7f89c99086f4c25
Veena Badveli <veenareddybadveli@gmail.com>
2025-12-22 13:18:35 +0530

    UI changes and bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Import all your components from the components folder
-import VormirexLanding from './Landingpage/VormirexLanding';
-import SoundFamiliar from './Landingpage/SoundFamiliar';
-import Whatisvormirex from './Landingpage/Whatisvormirex';
-import BuiltForEveryone from './Landingpage/BuiltForEveryone';
-import HowItWorks from './Landingpage/HowItWorks';
-import CourseGrid from './Landingpage/CourseGrid';
-import TestimonialSection from './Landingpage/TestimonialSection';
-import PricingSection from './Landingpage/PricingSection';
-import LearningCTA from './Landingpage/LearningCTA';
-import Footer from './Landingpage/Footer';
 
+import DashboardPage from './pages/DashboardPage';
+import LandingPage from './Landingpage/LandingPage';
 function App() {
   return (
-    <div className="app-wrapper">
-      <VormirexLanding />
-      <SoundFamiliar />
-      <Whatisvormirex />
-      <BuiltForEveryone />
-      <HowItWorks />
-      <CourseGrid />
-      <TestimonialSection />
-      <PricingSection />
-      <LearningCTA />
-      <Footer />
+    <Router>
+      <Routes>
+        {/* First page */}
+        <Route path="/" element={<DashboardPage />} />
 
-      <style>{`
-        .app-wrapper {
-          background-color: #0a0b14;
-          min-height: 100vh;
-          margin: 0;
-          padding: 0;
-          display: flex;
-          flex-direction: column;
-        }
-        
-        .app-wrapper > * {
-          flex-shrink: 0;
-        }
-      `}</style>
-    </div>
+        {/* Landing page */}
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
